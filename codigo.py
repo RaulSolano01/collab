c11ae60f7d9b2cc70c50010b52530e0d8b156651
Raul Solano <psolanos@upiic.mx>
2023-10-06 13:59:06 -0600

    se agrego todo

---
+++ codigo.py
@@ -0,0 +1,21 @@
+def es_primo(numero):
+    if numero <= 1:
+        return False  # Los números menores o iguales a 1 no son primos
+
+    # Comprobar si el número es divisible por cualquier número en el rango
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False  # El número es divisible, por lo que no es primo
+
+    return True  # Si no se encontraron divisores, el número es primo
+
+if __name__ == "__main__":
+    try:
+        numero = int(input("Ingrese un número entero mayor que 1: "))
+        if es_primo(numero):
+            print(f"{numero} es un número primo.")
+        else:
+            print(f"{numero} no es un número primo.")
+    except ValueError:
+        print("Por favor, ingrese un número entero válido mayor que 1.")
+
